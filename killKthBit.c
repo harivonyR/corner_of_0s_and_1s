1e690a24cfc60c18c9e94625cbf3e87d214ec915
Harivony RATEFIARISON <33831004+TensorEleven@users.noreply.github.com>
2020-11-20 22:00:32 +0300

    Update killKthBit.c
    debug comment remouved

--- killKthBit.c
+++ killKthBit.c
@@ -1,5 +1,4 @@
 /*
-	Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
 
 In order to stop the Mad Coder evil genius you need to decipher the encrypted message he sent to his minions. 
 
